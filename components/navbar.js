8165ae075b7c268cda86dfde06aed58dad7e6c0d
vzaleto <vzaleto@gmail.com>
2022-02-10 16:00:05 +0200

    mob navbar

--- components/navbar.js
+++ components/navbar.js
@@ -10,14 +10,19 @@ function Navbar () {
 		
 
 		return` <nav class="blue">
-		    <div class="nav-wrapper container">
-		      <a href="#" class="brand-logo right">BlogApp</a>
-		      <ul id="nav-mobile" class="left hide-on-med-and-down">
-		        <li><a href="index.html">Home</a></li>
-		        <li><a href="index.html?page=add">Add</a></li>
-		      </ul>
-		    </div>
-		  </nav>`
+		<div class="nav-wrapper container">
+		<a href="#" class="brand-logo right">BlogApp</a>
+		<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+		<ul id="nav-mobile" class="left hide-on-med-and-down">
+		<li><a href="index.html">Home</a></li>
+		<li><a href="index.html?page=add">Add</a></li>
+		</ul>
+		<ul class="sidenav" id="mobile-demo">
+		<li><a href="index.html">Home</a></li>
+		<li><a href="index.html?page=add">Add</a></li>
+		</ul>
+		</div>
+		</nav>`
 	}
 
 	return{
